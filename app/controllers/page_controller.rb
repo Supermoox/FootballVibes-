0b536ee8be8ad0d25a381c011201071eecdb264b
Mkusa Sakala <mooxmk@yahoo.co.uk>
2016-10-30 19:58:13 +0300

    bult nav added page controller

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,16 @@
+class PageController < ApplicationController
+  def index
+  end
+
+  def teams
+  end
+
+  def leagues
+  end
+
+  def fixtures
+  end
+
+  def features
+  end
+end
---
+++ test/controllers/page_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class PageControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get teams" do
+    get :teams
+    assert_response :success
+  end
+
+  test "should get leagues" do
+    get :leagues
+    assert_response :success
+  end
+
+  test "should get fixtures" do
+    get :fixtures
+    assert_response :success
+  end
+
+  test "should get features" do
+    get :features
+    assert_response :success
+  end
+
+end
